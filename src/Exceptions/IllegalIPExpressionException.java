e05660bcbc0984255588a9b8d8cae0847925dee7
whdudgns412 <33975263+whdudgns412@users.noreply.github.com>
2019-10-09 19:22:54 +0900

    0.6.10 update - cleaning

--- src/Exceptions/IllegalIPExpressionException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class IllegalIPExpressionException extends Exception{
-	IllegalIPExpressionException(String msg){
-		super(msg);
-	}
-}
---
+++ src/Game/GamePanel.java
@@ -0,0 +1,10 @@
+package Game;
+
+import javax.swing.JPanel;
+
+import Map.Engines.Layer;
+
+public class GamePanel extends JPanel{
+	private Layer blockLayer = new Layer();
+	private Layer spikeLayer = new Layer();
+}
